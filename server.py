a40e363fbd8ef2a0fd1b0f552626fd4212139a9d
ljs2225 <ljs2225@columbia.edu>
2025-08-15 12:04:59 -0400

    increase score immediately on button click without refreshing

--- server.py
+++ server.py
@@ -38,25 +38,21 @@ scoreboard = [
 
 @app.route('/')
 def show_scoreboard():
-    return render_template('scoreboard.html', scoreboard = scoreboard) 
+    return render_template('scoreboard.html', scoreboard = scoreboard)
 
 @app.route('/increase_score', methods=['GET', 'POST'])
 def increase_score():
     global scoreboard
 
-    json_data = request.get_json()   
-    team_id = json_data["id"]  
+    json_data = request.get_json()
+    team_id = json_data["id"]
     
     for team in scoreboard:
         if team["id"] == team_id:
             team["score"] += 1
-
+            
     return jsonify(scoreboard=scoreboard)
 
 
 if __name__ == '__main__':
    app.run(debug = True)
-
-
-
-
